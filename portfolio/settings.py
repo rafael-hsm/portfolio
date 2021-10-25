ababbd18f27b10ba79aa2494ffc011c9b112b453
Rafael Meireles <rafameireles2011@gmail.com>
2021-10-24 23:09:35 -0300

    Added app base

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.base'
+    'portfolio.base',
 ]
 
 MIDDLEWARE = [
